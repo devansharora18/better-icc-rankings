5fade85134ae12a37b9009e3c7852b479c2a8cae
Devansh Arora <hsnaved.reverse@gmail.com>
2024-08-04 14:58:47 +0530

    updated rankings

--- website/cricket-rankings/app/tests/test_ratings.ts
+++ website/cricket-rankings/app/tests/test_ratings.ts
@@ -1,14 +1,14 @@
 export const testRatings = [
 	{team: 'Australia', elo: 1861.22},
 	{team: 'India', elo: 1829.08},
-	{team: 'England', elo: 1741.79},
+	{team: 'England', elo: 1761.86},
 	{team: 'New Zealand', elo: 1680.38},
 	{team: 'South Africa', elo: 1658.84},
 	{team: 'Pakistan', elo: 1600.81},
 	{team: 'Sri Lanka', elo: 1578.06},
-	{team: 'West Indies', elo: 1524.37},
+	{team: 'West Indies', elo: 1504.30},
 	{team: 'Bangladesh', elo: 1370.45},
+	{team: 'Ireland', elo: 1364.93},
 	{team: 'Afghanistan', elo: 1363.30},
-	{team: 'Ireland', elo: 1353.35},
-	{team: 'Zimbabwe', elo: 1254.97},
+	{team: 'Zimbabwe', elo: 1243.39},
 ];
